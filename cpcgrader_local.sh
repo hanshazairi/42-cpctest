991ee2feea07b7a546e08bbaf35acf2ac7230125
Hans Hazairi <hanshazairi@gmail.com>
2021-03-17 17:59:00 +0800

    Added cpcgrader.sh & cpcgrader_local.sh

---
+++ cpcgrader_local.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+clear
+ROOTDIR=~/desktop/42kl/piscine
+echo -e "\033[36mSelect project to grade:\033[0m"
+select PROJECT in $(ls -d $ROOTDIR/cpc/*/ | sed 's_/$__' | sed 's_.*/__'); do
+	[[ -z $PROJECT ]] && { echo -e >&2 "\033[31mERROR:\033[0m Bad option"; exit 1; } || break
+done
+echo -e "\033[36mYou selected \033[33m$PROJECT\033[36m. Select exercise to grade:\033[0m"
+select EXERCISE in $(ls -d $ROOTDIR/cpc/$PROJECT/*/ | sed 's_/$__' | sed 's_.*/__'); do
+	[[ -z $EXERCISE ]] && { echo -e >&2 "\033[31mERROR:\033[0m Bad option"; exit 2; } || break
+done
+TESTDIR=$ROOTDIR/cpctest/$PROJECT/$EXERCISE
+WORKDIR=$ROOTDIR/cpc/$PROJECT/$EXERCISE
+cp $TESTDIR/* $WORKDIR
+gcc -Wall -Wextra -Werror $WORKDIR/*.c -o $WORKDIR/$EXERCISE
+$WORKDIR/$EXERCISE > $WORKDIR/user_output
+echo -e "\033[36mGrading \033[33m$PROJECT/$EXERCISE\033[36m..\033[0m"
+echo -en "\033[33m$PROJECT/$EXERCISE: \033[0m"
+cmp -s $WORKDIR/test_output $WORKDIR/user_output && echo -e "\033[32mOK\033[0m" || echo -e "\033[31mKO\033[0m"
+rm $WORKDIR/$EXERCISE $WORKDIR/main.c $WORKDIR/test_output $WORKDIR/user_output
--- test_local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-[ $# == 2 ] || { echo 'ERROR: Script takes 2 parameters!'; exit 1; }
-
-cp ~/desktop/42kl/piscine/cpctest/$1/$2/* $2/
-gcc -Wall -Wextra -Werror $2/*.c -o $2/$2
-./$2/$2 > $2/user_output
-cmp -s $2/test_output $2/user_output && echo "$2: OK" || echo "$2: KO"
-rm $2/$2 $2/main.c $2/test_output $2/user_output || { echo 'ERROR: Could not delete test files!'; exit 1; }
